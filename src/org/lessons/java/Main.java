01728bb7956af78655b088836d4212de02e57dfb
niccoloruberti <nikiruberti@gmail.com>
2023-11-17 16:12:07 +0100

    es 3 base completato

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,9 +1,19 @@
 package org.lessons.java;
 
+import org.lessons.java.pojo.ElencoInteri;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] numeriInteri = {1, 9, 6, 8, 5};
+		
+		ElencoInteri elenco = new ElencoInteri(numeriInteri, 0);
+		
+		while (elenco.hasAncoraElementi()) {
+			
+			System.out.println(elenco.getElementoSuccessivo());
+		}
 
 	}
 
--- src/org/lessons/java/pojo/ElencoInteri.java
+++ src/org/lessons/java/pojo/ElencoInteri.java
@@ -1,5 +1,51 @@
 package org.lessons.java.pojo;
 
 public class ElencoInteri {
+	
+	private int[] elencoInteri;
+	private int indice = 0;
+
+	//costruttore
+	public ElencoInteri(int[] elencoInteri, int indice) {
+		
+		setElencoInteri(elencoInteri);
+		setIndice(indice);
+		
+	}
+	
+	//getter e setter
+	public int[] getElencoInteri() {
+		return elencoInteri;
+	}
+
+	public void setElencoInteri(int[] elencoInteri) {
+		this.elencoInteri = elencoInteri;
+	}
+	
+	public int getIndice() {
+		return indice;
+	}
+
+	public void setIndice(int indice) {
+		this.indice = indice;
+	}
+	
+	//altri metodi
+	public int getElementoSuccessivo() {
+		
+		int elem = getElencoInteri()[indice];
+		indice++;
+		return elem;
+		
+	}
+	
+	public boolean hasAncoraElementi() {
+		
+		if (getIndice() < getElencoInteri().length ) {
+			return true;
+		}
+		
+		return false;
+	}
 
 }
